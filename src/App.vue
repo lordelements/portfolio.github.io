a236cfa774a785c1312b11ff3901f1da35f08181
Andrian Elemento <andrianelemento41@gmail.com>
2021-11-28 16:16:04 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,75 @@
 <template>
-   <ul></ul>
+  <div>
+  <portfolio
+            home=""
+            skill=""
+            projects=""
+            about=""
+            contacts=""
+            Name=""
+            MotherName=""
+           Email=""
+           Address=""
+           Birthday=""
+           GraduatedinElementary=""
+           GraduatedinHighschool=""
+           Working=""
+        ></portfolio><br>
+  <welcome msg="Welcome to Element's Personal Portfolio Website"/>
+  </div>
 </template>
 
 <script>
+
+// import Portfolio from './components/Portfolio'
+// import Welcome from './components/Welcome'
+
 export default {
+  name: 'App',
+  // components: {
+  //      Portfolio,
+  //      Welcome,
+      
+  // },
   data() {
     return {
-      Name: "Hello I'm Andrian Elemento",
-      hero: "Life is like proramming full of exploration and failure mistakes and error but in every error theres always solution to solve the problems and make it better",
+      contents: [ 
+        { 
+                      home: '',
+                      skill: '',
+                      projects: '',
+                      about: '',
+                      contacts: '',
+
+                  },
+                 ],
+                 aboutme: [
+                {
+                  Name: 'Andrian Elemento O.',
+                    MotherName: 'Leonisa Elemento',
+                    FatherName: 'Vergilio Elemento',
+                    Email: 'andri@gmail.com',
+                    Address: 'Biton Magallanes Sorsogon',
+                    Birthday: 'November 08, 1996',
+                    GraduatedinElementary: 'Biton Elementary School',
+                    GraduatedinHighschool: 'Biton National High School Batch: 2014-2015 from Old curiculum',
+                    Working: 'As Fisherman',
+
+                },
+            ]
     }
   },
-};
+}
 </script>
 
 <style>
-.brand a{
-    text-decoration: none;
-}.header{
-  position: fixed;
-  z-index: 1000;
-  left: 0;
-  top: 0;
-  width: 100vw;
-  height: 60px;
-  background-color: rgb(18, 216, 123);
-}
-.brand h1 {
-    font-size: 20px;
-    text-transform: uppercase;
-    color: #212121;
-    float: right;
-    margin: 10px 30px 30px 10px;
-  }
-  .brand h1 span {
-    color: rgb(231, 57, 14);
-  }
-.hero{
-    position: relative;
-    width: 100%;
-    
-}
-nav{
-    display: flex;
-    width: 84;
-    margin: auto;
-    align-items: center;
-    justify-content: space-between;
-}nav ul li{
-    display: inline-block;
-    list-style: none;
-    margin: 10px 20px;
-}nav ul li a{
- text-decoration: none;
- color: #000;
- font-weight: bold;
-}nav ul li a:hover{
-    color: crimson;
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #407fbd;
+  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import Welcome from './components/Welcome.vue'
+import Portfolio from './components/Portfolio.vue'
+import '@/styling/style.css';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.component('welcome', Welcome);
+app.component('portfolio', Portfolio);
+app.mount('#app');
